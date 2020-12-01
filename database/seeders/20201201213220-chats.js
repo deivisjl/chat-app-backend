0266cc99bdef5f7a5d4055dcfcb27ca59858a477
deivisjl <deivisjl@gmail.com>
2020-12-01 15:42:24 -0600

    Seeder de chats

---
+++ database/seeders/20201201213220-chats.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const models = require('../../models')
+const User = models.User
+const Chat = models.Chat
+const ChatUser = models.ChatUser
+const Message = models.Message
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const user = await User.findAll({ limit: 2 })
+   const chat = await Chat.create()
+   await ChatUser.bulkCreate([
+     {
+       chatId: chat.id,
+       userId: user[0].id
+     },
+     {
+        chatId: chat.id,
+        userId: user[1].id
+     },
+   ])
+   await Message.bulkCreate([
+     {
+       message: 'Hello friend',
+       chatId: chat.id,
+       fromUserId: user[0].id
+     },
+     {
+      message: 'Hi buddy',
+      chatId: chat.id,
+      fromUserId: user[1].id
+    },
+    {
+      message: 'Long time no speak',
+      chatId: chat.id,
+      fromUserId: user[1].id
+    },
+   ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

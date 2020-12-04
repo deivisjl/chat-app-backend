eed50f7d29e3528b5c7b94dd0529e8d3f1c7f118
deivisjl <deivisjl@gmail.com>
2020-12-04 12:04:01 -0600

    Agregando el socket server

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const cors = require('cors')
 
 const app = express()
 
+const http = require('http')
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use(cors())
@@ -19,6 +21,10 @@ app.use(express.static(__dirname + '/uploads'))
 
 const port = myConfig.appPort
 
-app.listen(port, ()=>{
-    console.log('Server listening on port 8081')
+const server = http.createServer(app)
+const SocketServer = require('./socket')
+SocketServer(server)
+
+server.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`)
 })

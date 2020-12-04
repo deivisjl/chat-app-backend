189ff6f10f05a97a6df93157980ab0579f815bcc
deivisjl <deivisjl@gmail.com>
2020-12-04 12:31:35 -0600

    Habilitando cors en el socket y evento de typing

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,20 @@
 const socketIo = require('socket.io')
 
 const SocketServer = (server) =>{
-    const io = socketIo(server)
+    const io = socketIo(server,{
+        cors:{
+            origin:'*',
+            methods:['GET','HEAD','PUT','PATCH','POST','DELETE'],
+            preflightContinue:false,
+            optionsSuccessStatus:204
+        }
+    })
 
     io.on('connection', (socket) =>{
 
         socket.on('join', async(user) =>{
             console.log("New user joined: ", user.firstName)
+            io.to(socket.id).emit('typing','User typing...')
         })
     })
 }

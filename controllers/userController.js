23b18fcdcd3b8efac242392e23038d64da6f7ce5
deivisjl <deivisjl@gmail.com>
2020-12-07 11:16:36 -0600

    Busqueda de registro en el api

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,6 +25,34 @@ exports.update = async(req, res) =>{
         return res.send(user)
 
     } catch (error) {
-        return res.status(500).json({error: e.message})
+        return res.status(500).json({error: error.message})
+    }
+}
+
+exports.search = async (req, res) =>{
+    try {
+        const users = await User.findAll({
+            where:{
+                [sequelize.Op.or] : {
+                    namaesConcated: sequelize.where(
+                        sequelize.fn('concat', sequelize.col('firstname'),' ', sequelize.col('lastName')),
+                        {
+                            [sequelize.Op.iLike] : `%${req.query.term}%`
+                        }
+                    ),
+                    email:{
+                        [sequelize.Op.iLike] : `%${req.query.term}%`
+                    }
+                },
+                [sequelize.Op.not] : {
+                    id: req.user.id
+                }
+            },
+            limit: 10
+        })
+
+        return res.json(users)
+    } catch (error) {
+        return res.status(500).json({error: error.message})
     }
 }
